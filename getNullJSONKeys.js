346ad1135afb205e3762a69d80915272e184f669
gh0$t <6196046+g-h-0-S-t@users.noreply.github.com>
2022-01-20 02:16:33 +0530

    Create getNullJSONKeys.js

---
+++ getNullJSONKeys.js
@@ -0,0 +1,72 @@
+javascript:
+
+/* MIT License
+ * 
+ * Copyright (c) 2022 gh0$t
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE. */
+
+function nestedLoop(obj) {
+    const res = {};
+    var emptyKey = [];
+
+    function recurse(obj, current) {
+
+        for (const key in obj) {
+            let value = obj[key];
+            if (value != undefined) {
+                if (value && typeof value === 'object') {
+                    recurse(value, key);
+                } else {
+                    res[key] = value;
+                }
+            } else {
+                emptyKey.push(key);
+            }
+        }
+    }
+    recurse(obj);
+    console.log('Null Valued Keys -> ', emptyKey);
+}
+
+
+
+function flattenObject(ob) {
+    var toReturn = {};
+
+    for (var i in ob) {
+        if (!ob.hasOwnProperty(i)) continue;
+
+        if ((typeof ob[i]) == 'object' && ob[i] !== null) {
+            var flatObject = flattenObject(ob[i]);
+            for (var x in flatObject) {
+                if (!flatObject.hasOwnProperty(x)) continue;
+
+                toReturn[i + '.' + x] = flatObject[x];
+            }
+        } else {
+            toReturn[i] = ob[i];
+        }
+    }
+    return toReturn;
+}
+
+var flatObj = flattenObject(x);
+
+nestedLoop(flatObj);
